3f63d24034ec6d899b75a0657276f2c1b8c7fb9d
GemmaTuron <gemma@ersilia.io>
2025-05-09 18:11:10 +0200

    update model

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM bentoml/model-server:0.11.0-py37
+FROM bentoml/model-server:0.11.0-py38
 MAINTAINER ersilia
 
 # RUN conda install -c conda-forge rdkit=2021.03.4
@@ -10,4 +10,4 @@ RUN pip install scipy==1.7.1
 RUN pip install scikit-learn==0.24.2
 
 WORKDIR /repo
-COPY ./repo
+COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,89 +1,30 @@
-import random
-import sys
+# imports
 import os
-import numpy as np
-import torch
-import pandas as pd
-from rdkit import RDLogger
-from pathlib import Path
-import tempfile
+import csv
+import sys
+from grover.predict import grover_predict
 
-from grover.util.parsing import parse_args, get_newest_train_args
-from grover.util.utils import create_logger
-from task.cross_validate import cross_validate
-from task.fingerprint import generate_fingerprints
-from task.predict import make_predictions, write_prediction
-from task.pretrain import pretrain_model
-from grover.data.torchvocab import MolVocab
-import scripts.save_features as sf
-
-class Namespace:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-def setup(seed):
-    # frozen random seed
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
-
-def smiles_to_dataframe(txt_file_path):
-
-    
-    df = pd.read_csv(txt_file_path, header=None,  names=['smiles'])
-    
-    dummy_labels = pd.Series(np.zeros(df.shape[0]))
-
-    names =    ['NR-AR',	
-                'NR-AR-LBD',
-                'NR-AhR',
-                'NR-Aromatase',
-                'NR-ER',
-                'NR-ER-LBD',
-                'NR-PPAR-gamma',
-                'SR-ARE',
-                'SR-ATAD5',
-                'SR-HSE',
-                'SR-MMP',
-                'SR-p53']
-    
-    for n in names:
-        df[n] = dummy_labels.values
-
-    input_csv_path = txt_file_path.split(".")[0] + ".csv"
-    df.to_csv(input_csv_path, index=False)
-    
-    return input_csv_path
-
-tmp_folder = tempfile.mktemp()
-features_path = os.path.join(tmp_folder, "features.npz")  
 
 if __name__ == '__main__':
-    # setup random seed
-    setup(seed=42)
-    # Avoid the pylint warning.
-    a = MolVocab
-    # supress rdkit logger
-    lg = RDLogger.logger()
-    lg.setLevel(RDLogger.CRITICAL)
+    # parse arguments
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-    # Initialize MolVocab
-    mol_vocab = MolVocab
+    # current file directory
+    root = os.path.dirname(os.path.abspath(__file__))
 
-    input_txt_path =  sys.argv[1]
-    output_path = sys.argv[2]
-    csv_path = smiles_to_dataframe(input_txt_path)
+    #No need to read the smiles from the main.py , the model will read itself.
+    outputs, read_smiles = grover_predict(input_txt_path=input_file, output_path=output_file)
 
-    s = os.path.dirname(os.path.abspath(__file__))
-    p = Path(s)
-    model_path = str(p.parent.parent.absolute())
+    #check input and output have the same lenght
+    input_len = len(read_smiles)
+    output_len = len(outputs)
+    assert input_len == output_len
+    cols = ['nr-ar', 'nr-ar-lbd', 'nr-ahr', 'nr-aromatase', 'nr-er', 'nr-er-lbd', 'nr-ppar-gamma', 'sr-are', 'sr-atad5', 'sr-hse', 'sr-mmp', 'sr-p53']
 
-    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/tox21', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/tox21/fold_0/model_0/model.pt', model_path+'/framework/finetune/tox21/fold_2/model_0/model.pt', model_path+'/framework/finetune/tox21/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=[features_path], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
-    sf.save_features_main(csv_path,features_path)
-    train_args = get_newest_train_args()
-    avg_preds, test_smiles = make_predictions(args, train_args)
-    write_prediction(avg_preds, test_smiles, args)
-
-    os.remove(csv_path)
+    # write output in a .csv file
+    with open(output_file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(cols)  # header
+        for o in outputs:
+            writer.writerow(o)
